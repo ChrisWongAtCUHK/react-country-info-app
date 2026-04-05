69442f1e2a7c2662d3315d335b6d29fc13085b73
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-05 11:09:21 +0800

    Install and use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import Countries from './Countries.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/all" element={<Countries />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
